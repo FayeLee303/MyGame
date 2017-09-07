e20f975f5fabd69e6b1daf14b19e2acfb3695e13
FayeLee303 <1653090976@qq.com>
2017-09-07 14:45:33 +0800

    添加字典扩展类
    类和方法都定义为Static，方法的第一个参数要用this修饰

--- Assets/Demo/Scripts/Manager/UIPanelManager.cs
+++ Assets/Demo/Scripts/Manager/UIPanelManager.cs
@@ -25,22 +25,75 @@ public class UIPanelManager : MonoBehaviour
         }
     }
 
+    //画布
+    private Transform canvasTransform;
+
+    private Transform CanvasTransform
+    {
+        get
+        {
+            if (canvasTransform == null)
+            {
+                canvasTransform = GameObject.Find("Canvas").transform;
+            }
+            return canvasTransform;
+        }
+    }
     //用字典储存所有面板prefab的路径
     private Dictionary<UIPanelType, string> panelPathDic;
-
+    //用字典储存所有实例化的面板的游戏物体上的BasePanel组件
+    private Dictionary<UIPanelType, BasePanel> panelDic;
+   
     //私有的构造方法
     private UIPanelManager ()
     {
         //ParseUIPanelTypeJson();
     }
-
+    /// <summary>
+    /// 初始化
+    /// </summary>
     public void Init()
     { 
         ParseUIPanelTypeJson();
     }
 
-    
-    //解析Json文件
+    /// <summary>
+    /// 根据面板类型，得到实例化的面板
+    /// </summary>
+    /// <returns></returns>
+    public BasePanel GetPanel(UIPanelType panelType)
+    {
+        if (panelDic == null)
+        {
+            panelDic = new Dictionary<UIPanelType, BasePanel>();//创建空字典
+
+        }
+        //根据传过来的面板类型去面板字典里找
+        //BasePanel panel;
+        //panelDic.TryGetValue(panelType, out panel);//TODO
+        BasePanel panel = panelDic.TryGet(panelType);
+        if (panel == null)
+        {
+            //如果找不到，就在面板路径字典里找prefab的路径
+            //string path;
+            //panelPathDic.TryGetValue(panelType, out path);
+            string path = panelPathDic.TryGet(panelType);
+            //根据prefab的路径实例化物体
+            GameObject instPanel = GameObject.Instantiate(Resources.Load(path)) as GameObject;
+            //放到画布下面
+            instPanel.transform.SetParent(canvasTransform); //TODO
+            panelDic.Add(panelType, instPanel.GetComponent<BasePanel>());
+            return instPanel.GetComponent<BasePanel>();
+        }
+        else
+        {
+            return panel;
+        }
+    }
+
+    /// <summary>
+    /// 解析Json文件
+    /// </summary>
     private void ParseUIPanelTypeJson()
     {
         panelPathDic = new Dictionary<UIPanelType, string>();//把字典置空
@@ -60,11 +113,10 @@ public class UIPanelManager : MonoBehaviour
         {
             Debug.Log("读取文件失败");
         }
-
-        string path;
-        panelPathDic.TryGetValue(UIPanelType.ItemPanel, out path);
-        Debug.Log(path);
-        //Debug.Log("555555");
+        //string path;
+        //panelPathDic.TryGetValue(UIPanelType.ItemPanel, out path);
+        //Debug.Log(path);
+        ////Debug.Log("555555");
     }
 
 }
